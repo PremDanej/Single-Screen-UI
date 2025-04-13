08a1a37cbffb715d02aa9a1da85a41637c495056
PremDanej <danejprem005@gmail.com>
2025-04-13 12:07:06 +0530

    SignupScreenPreview added.

--- app/src/main/java/com/merp/jet/single/ui/screen/MainActivity.kt
+++ app/src/main/java/com/merp/jet/single/ui/screen/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.merp.jet.single.ui.screen.signin.SignInScreenPreview
+import com.merp.jet.single.ui.screen.signup.SignUpScreenPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SignInScreenPreview()
+            //SignInScreenPreview()
+            SignUpScreenPreview()
         }
     }
 }
